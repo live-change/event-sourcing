752f682fc5211cbd5d86d1334c968b499a7d9fbe
Michał Łaszczewski <michal@laszczewski.com>
2020-04-07 10:52:07 +0200

    bug fix

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const EventSourcing = require('./lib/EventSourcing.js')
+const EventsReader = require('./lib/EventsReader.js')
+const CommandQueue = require('./lib/CommandQueue.js')
+const ExecutionQueue = require('./lib/ExecutionQueue.js')
+const KeyBasedExecutionQueues = require('./lib/KeyBasedExecutionQueues.js')
 
 module.exports = {
-
+  EventSourcing,
+  EventsReader,
+  CommandQueue,
+  ExecutionQueue,
+  KeyBasedExecutionQueues
 }
